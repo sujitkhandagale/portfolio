94fe1a6cdc61144a111b5eae5c1870892c3994d9
sujit khandagale <sujitkh94@outlook.com>
2024-03-31 16:27:24 +0530

    project created

--- components/home/technology/Technology.jsx
+++ components/home/technology/Technology.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import style from "./tech.module.scss";
 
 function Technology() {
   return (
     <div>
       <h2>Technology i use</h2>
+      <div className={style.technologies}>
+        <div className={style.tech}>
+
+        </div>
+      </div>
     </div>
   );
 }
--- pages/api/test.js
+++
@@ -1,7 +0,0 @@
-// write a simple api
-// connect to db
-import cachedConnection from "@/lib/mongoose";
-
-export default async function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
